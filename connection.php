51b53b2190125f8066a0b0af69de3be8cc6633b3
muhiasamuel <49280770+muhiasamuel@users.noreply.github.com>
2020-02-25 05:58:44 -0500

    samtecc
    my test crud api

---
+++ connection.php
@@ -0,0 +1,6 @@
+<?php
+
+$connect = new PDO("mysql:host =localhost; dbname = crud", "root","")
+or die("could not connect");
+
+  ?>
---
+++ fetch.php
@@ -0,0 +1,49 @@
+<?php 
+
+require_once ('connection.php');
+
+$query = "SELECT * FROM tbl_student ORDER BY id";
+
+$statement = $connect->prepare($query);
+
+$statement->execute();
+
+$result = $statement->fetchAll();
+$output = '';
+$total_row = $statement->rowCount();
+$output .= '<table class="table table-bordered table-stripped">
+     
+      <tr>
+        <th>first_name</th>
+        <th>last_name</th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </tr>
+      ';
+
+if($total_row > 0){
+
+	foreach ($result as $row) {
+	
+	$output .='
+	<tr>
+		<td>'.$row["first_name"].'</td>
+		<td>'.$row["last_name"].'</td>
+		<td><button type = "button" name = "edit" class = "btn btn-primary btn-xs edit" id = "'.$row["id"].'">Edit</button></td>
+		<td><button type = "button" name = "delete" class = "btn btn-danger btn-xs delete" id = "'.$row["id"].'">Delete</button></td>
+	</tr>
+	';
+}
+}
+else{
+	$output .='
+	<tr>
+		<td colspan="4" text align"center">No Data Found</td>
+	</tr>';
+}
+
+$output.= '</table>';
+
+echo $output;
+
+ ?>
