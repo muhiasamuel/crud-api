39496d58d31f069122cd34971ac62f5cfb996abe
ngangavic <ngangavictor10@gmail.com>
2020-02-25 16:26:48 +0300

    insert done

--- action.php
+++ action.php
@@ -5,9 +5,9 @@ if (isset($_POST["first_name"])&&isset($_POST["last_name"])){
     $stmt=$connect->prepare("INSERT INTO tbl_student(first_name,last_name)VALUES(?,?)");
     $stmt->bind_param("ss",$_POST["first_name"],$_POST["last_name"]);
     if (!$stmt->execute()){
-        echo"data could not be inserted";
+        echo "data could not be inserted";
     }else{
-        echo'<p>Data Inserted</p>';
+        echo '<p>Data Inserted</p>';
     }
 
 }
