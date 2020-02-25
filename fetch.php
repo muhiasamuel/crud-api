f9eed5e9c2267a52bac3ce7e41a6f792d244e5c7
ngangavic <ngangavictor10@gmail.com>
2020-02-25 16:38:59 +0300

    fetch done

--- fetch.php
+++ fetch.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 
-require_once ('connection.php');
+require_once('connection.php');
 
 $query = "SELECT * FROM tbl_student ORDER BY id";
 
@@ -8,9 +8,9 @@ $statement = $connect->prepare($query);
 
 $statement->execute();
 
-$result = $statement->fetchAll();
+$result = $statement->get_result();
 $output = '';
-$total_row = $statement->rowCount();
+$total_row = $result->num_rows;
 $output .= '<table class="table table-bordered table-stripped">
      
       <tr>
@@ -21,29 +21,28 @@ $output .= '<table class="table table-bordered table-stripped">
       </tr>
       ';
 
-if($total_row > 0){
+if ($total_row > 0) {
 
-	foreach ($result as $row) {
-	
-	$output .='
+    while ($row = $result->fetch_array()) {
+
+        $output .= '
 	<tr>
-		<td>'.$row["first_name"].'</td>
-		<td>'.$row["last_name"].'</td>
-		<td><button type = "button" name = "edit" class = "btn btn-primary btn-xs edit" id = "'.$row["id"].'">Edit</button></td>
-		<td><button type = "button" name = "delete" class = "btn btn-danger btn-xs delete" id = "'.$row["id"].'">Delete</button></td>
+		<td>' . $row["first_name"] . '</td>
+		<td>' . $row["last_name"] . '</td>
+		<td><button type = "button" name = "edit" class = "btn btn-primary btn-xs edit" id = "' . $row["id"] . '">Edit</button></td>
+		<td><button type = "button" name = "delete" class = "btn btn-danger btn-xs delete" id = "' . $row["id"] . '">Delete</button></td>
 	</tr>
 	';
-}
-}
-else{
-	$output .='
+    }
+} else {
+    $output .= '
 	<tr>
 		<td colspan="4" text align"center">No Data Found</td>
 	</tr>';
 }
 
-$output.= '</table>';
+$output .= '</table>';
 
 echo $output;
 
- ?>
+?>
