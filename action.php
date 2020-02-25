c377d241f0ed5fa63c0fc9a924c9fa7b4fc5d4df
ngangavic <ngangavictor10@gmail.com>
2020-02-25 15:58:19 +0300

    change from pdo to mysqli

--- action.php
+++ action.php
@@ -1,17 +1,15 @@
 <?php
 require_once ('connection.php');
 
-if(isset($_POST['action'])){
-    if($_POST["action"] == "insert"){
-        $query = "INSERT INTO tbl_student(first_name,last_name) 
-        VALUES('".$_POST["first_name"]."','".$_POST["last_name"]."')";
-        $statement = $connect->prepare($query);
-        $statement->execute();
-
-        echo'<p>Data Inserted</p>';
-    }else{
+if (isset($_POST["first_name"])&&isset($_POST["last_name"])){
+    $stmt=$connect->prepare("INSERT INTO tbl_student(first_name,last_name)VALUES(?,?)");
+    $stmt->bind_param("ss",$_POST["first_name"],$_POST["last_name"]);
+    if (!$stmt->execute()){
         echo"data could not be inserted";
+    }else{
+        echo'<p>Data Inserted</p>';
     }
+
 }
 
 ?>
